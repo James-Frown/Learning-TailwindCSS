b57f35123c5bccd34c8e3e2934fcc87ce969c3f8
James-Frown <21122@ctca.co.za>
2023-05-24 18:23:04 +0200

    additional changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <h1>
+        Vite + React + Tailwind
+      </h1>
+      <p>
+        This is a basic template file to use tailwindCSS
       </p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
