ac842cb27e03410b75d9ccb966376c1b396aacdd
James-Frown <21122@ctca.co.za>
2023-05-27 20:30:36 +0200

    changes tailwindcss to vite | react project

--- src/components/cheatsheet.jsx
+++ src/components/cheatsheet.jsx
@@ -1,4 +1,4 @@
-function flexer() {
+function cheatsheet() {
     return (
       <>
         <div className="
@@ -23,4 +23,4 @@ function flexer() {
     )
   }
   
-  export default flexer;
+  export default cheatsheet;
